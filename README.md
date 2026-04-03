1ee5fb535bd2d28a1c6c7df94b8ce4940ccd1962
Blockky <blockygh6@hotmail.com>
2026-04-03 02:00:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PECL Programación Avanzada 2026
+
+Proyecto de programación concurrente y distribuida en Java
+
+Desarrollado con JDK 25 en Apache NetBeans 28
---
+++ src/main/java/PECLProgConc2026.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Blockky
+ */
+public class PECLProgConc2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
