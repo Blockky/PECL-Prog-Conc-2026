652cc792b0cbeb686bf5ff3008f25445233cf49f
Blockky <blockygh6@hotmail.com>
2026-04-03 02:33:50 +0200

    Clase main de ejemplo

--- src/main/java/PECLProgAvan2026.java
+++ src/main/java/PECLProgAvan2026.java
@@ -2,9 +2,9 @@
  *
  * @author Blockky
  */
-public class PECLProgConc2026 {
+public class PECLProgAvan2026 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hell nah");
     }
 }
